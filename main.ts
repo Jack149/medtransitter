2cca34d5a71df575d27f2af78a1b031f9f2388b0
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-03-12 16:26:30 -0600

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,20 +1,33 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         basic.showIcon(IconNames.Happy)
+        basic.pause(2000)
+        basic.clearScreen()
     }
     if (receivedNumber == 1) {
         basic.showIcon(IconNames.Sad)
+        basic.pause(2000)
+        basic.clearScreen()
     }
     if (receivedNumber == 2) {
+        basic.showIcon(IconNames.Yes)
         music.setBuiltInSpeakerEnabled(true)
         music.setVolume(255)
         soundExpression.giggle.play()
+        basic.pause(2000)
+        basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
     basic.showIcon(IconNames.Happy)
     basic.pause(5000)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(2)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
     basic.clearScreen()
 })
 input.onGesture(Gesture.TiltLeft, function () {
