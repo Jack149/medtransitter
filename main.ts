4352baa0d78b807d21771d523dd485745fc8c3e9
Jack149 <31501037+Jack149@users.noreply.github.com>
2021-03-22 00:53:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,9 +20,8 @@ radio.onReceivedNumber(function (receivedNumber) {
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
-    basic.showIcon(IconNames.Happy)
-    basic.pause(5000)
-    basic.clearScreen()
+    soundExpression.hello.play()
+    basic.showString("HELP")
 })
 input.onGesture(Gesture.LogoUp, function () {
     radio.sendNumber(2)
@@ -31,16 +30,25 @@ input.onGesture(Gesture.LogoUp, function () {
     basic.clearScreen()
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(2)
+    radio.sendNumber(3)
     basic.showIcon(IconNames.Heart)
     basic.pause(2000)
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
-    basic.showIcon(IconNames.Sad)
+    soundExpression.happy.play()
+    basic.showString("DINNER")
     basic.pause(5000)
     basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    input.setAccelerometerRange(AcceleratorRange.FourG)
+    radio.sendNumber(4)
+    for (let index = 0; index < 4; index++) {
+        music.playMelody("C5 A B G A F G E ", 120)
+        basic.pause(500)
+    }
 })
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(2)
@@ -48,7 +56,14 @@ input.onGesture(Gesture.TiltRight, function () {
     basic.pause(2000)
     basic.clearScreen()
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendNumber(5)
+    for (let index = 0; index < 4; index++) {
+        basic.showString("WATER ")
+    }
+})
 radio.setTransmitPower(7)
+music.setBuiltInSpeakerEnabled(true)
 basic.forever(function () {
 	
 })
